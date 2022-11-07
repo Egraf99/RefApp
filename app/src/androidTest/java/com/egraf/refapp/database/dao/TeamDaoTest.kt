b1590a0ea7b581a73ff44e3baa500725e3eb5e89
Egraf99 <egorkhodin@gmail.com>
2022-11-07 16:05:21 +0300

    добавление Hilt

--- app/src/androidTest/java/com/egraf/refapp/database/dao/TeamDaoTest.kt
+++ app/src/androidTest/java/com/egraf/refapp/database/dao/TeamDaoTest.kt
@@ -1,38 +1,41 @@
 package com.egraf.refapp.database.dao
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.egraf.refapp.database.GameDatabase
 import com.egraf.refapp.database.entities.Team
 import com.egraf.refapp.getOrAwaitValue
 import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+import javax.inject.Inject
+import javax.inject.Named
 
-@RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 @SmallTest
+@HiltAndroidTest
 class TeamDaoTest {
 
     @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
-    private lateinit var database: GameDatabase
+
+    @Inject
+    @Named("test_db")
+    lateinit var database: GameDatabase
     private lateinit var teamDao: TeamDao
 
     @Before
     fun setup() {
-        database = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(),
-            GameDatabase::class.java
-        ).allowMainThreadQueries().build()
+        hiltRule.inject()
         teamDao = database.teamDao()
     }
 
