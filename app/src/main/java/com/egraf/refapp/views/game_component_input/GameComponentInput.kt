cc555aef04c242c10393e4069744db7c3c30b636
Egor Khodin <egorkhodin@gmail.com>
2022-12-12 15:58:03 +0300

    добавление обратной анимации

--- app/src/main/java/com/egraf/refapp/views/game_component_input/GameComponentInput.kt
+++ app/src/main/java/com/egraf/refapp/views/game_component_input/GameComponentInput.kt
@@ -9,9 +9,9 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.egraf.refapp.R
+import java.util.*
 
 
 enum class GameComponent(val value: Int,
@@ -37,12 +37,17 @@ enum class GameComponent(val value: Int,
 }
 
 class GameComponentInput(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs), View.OnClickListener {
-    private val mState: Int
+    private var isEmpty: Boolean
     private val gameComponent: GameComponent
     private val startIcon: Drawable?
     private val animTextView: TextView
     private val helpTextView: TextView
     private val contentTextView: TextView
+
+    private val appearAnim: Animation
+    private val disappearAnim: Animation
+    private val leftUpAnim: Animation
+    private val rightDownAnim: Animation
 
     init {
         context.theme.obtainStyledAttributes(
@@ -52,7 +57,7 @@ class GameComponentInput(context: Context, attrs: AttributeSet) : ConstraintLayo
         ).apply {
 
             try {
-                mState = getInteger(R.styleable.GameComponentInput_state, 0)
+                isEmpty = !getBoolean(R.styleable.GameComponentInput_fill, false)
                 gameComponent =
                     GameComponent.getComponent(
                         getInteger(
@@ -82,59 +87,128 @@ class GameComponentInput(context: Context, attrs: AttributeSet) : ConstraintLayo
         contentTextView = getChildAt(4) as TextView
 
         // заполняем text views
-        helpTextView.text = context.getText(gameComponent.title)
-        animTextView.text = context.getText(gameComponent.title)
+        val title = gameComponent.title
+        helpTextView.text = context.getText(title)
+        animTextView.text = context.getText(title)
         contentTextView.text = "Dinamo"
+
+        // init animations
+        appearAnim = AnimationUtils.loadAnimation(context, R.anim.appeare_textview).apply {
+            setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {}
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    contentTextView.visibility = View.VISIBLE
+                }
+
+                override fun onAnimationRepeat(animation: Animation?) {}
+            }
+            )
+        }
+        disappearAnim = AnimationUtils.loadAnimation(context, R.anim.disappeare_textview).apply {
+            setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {}
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    contentTextView.visibility = View.INVISIBLE
+                }
+
+                override fun onAnimationRepeat(animation: Animation?) {}
+            }
+            )
+        }
+        leftUpAnim = AnimationUtils.loadAnimation(context, R.anim.move_left_up).apply {
+            setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {}
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    animTextView.visibility = View.INVISIBLE
+                    helpTextView.visibility = View.VISIBLE
+                }
+
+                override fun onAnimationRepeat(animation: Animation?) {}
+            })
+        }
+        rightDownAnim = AnimationUtils.loadAnimation(context, R.anim.move_right_down).apply {
+            setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {}
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    animTextView.visibility = View.VISIBLE
+                    helpTextView.visibility = View.INVISIBLE
+                }
+
+                override fun onAnimationRepeat(animation: Animation?) {}
+            })
+        }
 
         val bracketClose = getChildAt(childCount - 1) as ImageView
         bracketClose.setBackgroundResource(R.drawable.ic_close_bracket)
 
         // clickable
         setOnClickListener(this)
+
+        // set content
+        setState(isEmpty)
     }
 
-    fun getStatus(): Int {
-        return mState
-    }
+    private fun fill() {
+        setContentEmpty()
 
-//    fun setStatus(state: Int) {
-//        mState = state
-//        invalidate()
-//        requestLayout()
-//    }
-
-    override fun onClick(v: View?) {
-        helpTextView.visibility = View.INVISIBLE
-        contentTextView.visibility = View.INVISIBLE
-        animTextView.visibility = View.VISIBLE
-
-        val scaleAnim = AnimationUtils.loadAnimation(context, R.anim.on_touch_textview_anim)
-        scaleAnim.setAnimationListener(object : Animation.AnimationListener {
-            override fun onAnimationStart(animation: Animation?) { }
-
-            override fun onAnimationEnd(animation: Animation?) {
-                animTextView.visibility = View.GONE
-                helpTextView.visibility = View.VISIBLE
-            }
-
-            override fun onAnimationRepeat(animation: Animation?) { }
-        })
-        val appearAnim = AnimationUtils.loadAnimation(context, R.anim.appearance_textview)
-        appearAnim.setAnimationListener(object : Animation.AnimationListener {
-            override fun onAnimationStart(animation: Animation?) { }
-
-            override fun onAnimationEnd(animation: Animation?) {
-                contentTextView.visibility = View.VISIBLE
-            }
-
-            override fun onAnimationRepeat(animation: Animation?) { }
-        })
-        scaleAnim.reset()
+        leftUpAnim.reset()
         appearAnim.reset()
         animTextView.clearAnimation()
-        animTextView.startAnimation(scaleAnim)
+        animTextView.startAnimation(leftUpAnim)
 
         contentTextView.clearAnimation()
         contentTextView.startAnimation(appearAnim)
+    }
+
+    private fun empty() {
+        setContentFill()
+
+        rightDownAnim.reset()
+        disappearAnim.reset()
+        helpTextView.clearAnimation()
+        helpTextView.startAnimation(rightDownAnim)
+
+        contentTextView.clearAnimation()
+        contentTextView.startAnimation(disappearAnim)
+    }
+
+    private fun setContentEmpty() {
+        helpTextView.visibility = View.INVISIBLE
+        contentTextView.visibility = View.INVISIBLE
+        animTextView.visibility = View.VISIBLE
+    }
+
+    private fun setContentFill() {
+        helpTextView.visibility = View.VISIBLE
+        contentTextView.visibility = View.VISIBLE
+        animTextView.visibility = View.INVISIBLE
+    }
+
+    private fun setState(state: Boolean) {
+        if (state) {
+            setContentEmpty()
+        } else {
+            setContentFill()
+        }
+    }
+
+    private fun changeState() {
+        if (isEmpty) {
+            isEmpty = false
+            fill()
+        } else {
+            isEmpty = true
+            empty()
+        }
+
+    }
+
+
+    override fun onClick(v: View?) {
+        changeState()
     }
 }
