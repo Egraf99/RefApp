838bd604f64c001a7742fd1c69b8f48629bf51b6
Egor Khodin <egorkhodin@gmail.com>
2023-02-18 11:51:21 +0300

    добавлен тест нажатия на кнопку добаления игры

--- app/src/androidTest/java/com/egraf/refapp/di/TestAppModule.kt
+++ app/src/androidTest/java/com/egraf/refapp/di/TestAppModule.kt
@@ -1,23 +1,23 @@
 package com.egraf.refapp.di
-
-import android.content.Context
-import androidx.room.Room
-import com.egraf.refapp.database.local.GameDatabase
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Named
-
-@Module
-@InstallIn(SingletonComponent::class)
-object TestAppModule {
-    @Provides
-    @Named("test_db")
-    fun provideInMemoryDb(@ApplicationContext context: Context) =
-        Room.inMemoryDatabaseBuilder(
-            context,
-            GameDatabase::class.java
-        ).allowMainThreadQueries().build()
-}
+//
+//import android.content.Context
+//import androidx.room.Room
+//import com.egraf.refapp.database.local.GameDatabase
+//import dagger.Module
+//import dagger.Provides
+//import dagger.hilt.InstallIn
+//import dagger.hilt.android.qualifiers.ApplicationContext
+//import dagger.hilt.components.SingletonComponent
+//import javax.inject.Named
+//
+//@Module
+//@InstallIn(SingletonComponent::class)
+//object TestAppModule {
+//    @Provides
+//    @Named("test_db")
+//    fun provideInMemoryDb(@ApplicationContext context: Context) =
+//        Room.inMemoryDatabaseBuilder(
+//            context,
+//            GameDatabase::class.java
+//        ).allowMainThreadQueries().build()
+//}
--- app/src/main/java/com/egraf/refapp/database/local/source/FakeGameDataSource.kt
+++ app/src/main/java/com/egraf/refapp/database/local/source/FakeGameDataSource.kt
@@ -1,107 +1,186 @@
 package com.egraf.refapp.database.local.source
 
-//class FakeGameDataSource(private val initFakeData: Boolean = false) : GameDataSource {
-//    private val games = mutableListOf<GameWithAttributes>()
-//    private val teams = mutableListOf<Team>()
-//    private val stadiums = mutableListOf<Stadium>()
-//    private val leagues = mutableListOf<League>()
-//    private val referees = mutableListOf<Referee>()
-//    val fillFakeData = {
-//        val fakeGame = Game(id = fakeUUID)
-//        addGame(fakeGame)
-//    }
-//
-//    init {
-//        if (initFakeData) fillFakeData()
-//    }
-//
-//
-//    // games block
-//    override fun getGames(): LiveData<List<GameWithAttributes>> = MutableLiveData(games)
-//    override fun countGames(): LiveData<Int> = MutableLiveData(games.size)
-//    override fun getGame(id: UUID): LiveData<GameWithAttributes?> {
-//        games.forEach { game -> if (game.game.id == id) return MutableLiveData(game) }
-//        return MutableLiveData(null)
-//    }
-//
-//    override fun updateGame(game: Game) {
-////        games.value?.forEachIndexed { index, gameWithAttributes ->
-////            if (gameWithAttributes.game.id == game.id) {
-////                games.value?.removeAt(index)
-////                games.value?.add(index, gameWithAttributes)
-////            }
-////        }
-//    }
-//
-//    override fun addGame(game: Game) {
-//        games.add(GameWithAttributes(game = game))
-////        val games_ = games.value as MutableList
-////        games_.add(GameWithAttributes(game = game))
-////        games.postValue(games_)
-//    }
-//
-//    override fun deleteGame(game: Game) {
-////        games.value?.forEachIndexed { index, gameWithAttributes ->
-////            if (gameWithAttributes.game.id == game.id) {
-////                games.value?.removeAt(index)
-////            }
-////        }
-//    }
-//
-//    // stadium block
-//    override fun addStadium(stadium: Stadium) {
-//        stadiums.add(stadium)
-//    }
-//    override fun getStadiums(): List<Stadium> = listOf()
-//    override fun getStadium(id: UUID): Stadium? {
-//        return Stadium()
-////        stadiums.forEach { stadium -> if (stadium.id == id) return MutableLiveData(stadium) }
-////        return MutableLiveData(null)
-//    }
-//    override fun updateStadium(stadium: Stadium) {
-//        stadiums.forEachIndexed { index, stadium_ ->
-//            if (stadium_.id == stadium.id) {
-//                stadiums.removeAt(index)
-//                stadiums.add(index, stadium)
-//            }
-//        }
-//    }
-//
-//    // league block
-//    override fun getLeagues(): LiveData<List<League>> = MutableLiveData(leagues)
-//    override fun addLeague(league: League) {
-//        leagues.add(league)
-//    }
-//    override fun updateLeague(league: League) {
-//        leagues.forEachIndexed { index, league_ ->
-//            if (league_.id == league.id) {
-//                leagues.removeAt(index)
-//                leagues.add(index, league)
-//            }
-//        }
-//    }
-//
-//    // team block
-//    override fun getTeams(): LiveData<List<Team>> = MutableLiveData(teams)
-//    override fun deleteTeam(team: Team) {
-//        teams.forEachIndexed { index, team_ ->
-//            if (team_.id == team.id) {
-//                teams.removeAt(index)
-//            }
-//        }
-//    }
-//
-//    override fun addTeam(team: Team) {
-//        teams.add(team)
-//    }
-//
-//    // referee block
-//    override fun getReferees(): LiveData<List<Referee>> = MutableLiveData(referees)
-//    override fun addReferee(referee: Referee) {
-//        referees.add(referee)
-//    }
-//
-//    companion object {
-//        val fakeUUID = UUID.randomUUID()
-//    }
-//}
+import androidx.lifecycle.LiveData
+import com.egraf.refapp.database.local.entities.*
+import com.egraf.refapp.database.remote.model.open_weather_pojo.WeatherResponse
+import kotlinx.coroutines.flow.Flow
+import retrofit2.Call
+import java.util.*
+
+class FakeGameDataSource() : GameDataSource {
+    override fun getGames(): Flow<List<GameWithAttributes>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun countGames(): LiveData<Int> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getGame(id: UUID): Flow<GameWithAttributes?> {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateGame(game: Game) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateHomeTeamInGame(gameId: UUID, teamId: UUID) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateGuestTeamInGame(gameId: UUID, teamId: UUID) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateStadiumInGame(gameId: UUID, stadiumId: UUID) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateLeagueInGame(gameId: UUID, leagueId: UUID) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateChiefRefereeInGame(gameId: UUID, refereeId: UUID) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateFirstAssistantInGame(gameId: UUID, refereeId: UUID) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateSecondAssistantInGame(gameId: UUID, refereeId: UUID) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateReserveRefereeInGame(gameId: UUID, refereeId: UUID) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateInspectorInGame(gameId: UUID, refereeId: UUID) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateDateTimeInGame(gameId: UUID, dt: GameDateTime) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updatePassedGame(gameId: UUID, isPassed: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updatePaidGame(gameId: UUID, isPaid: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun addGame(game: Game) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteGame(game: Game) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteGame(gameId: UUID) {
+        TODO("Not yet implemented")
+    }
+
+    override fun addStadium(stadium: Stadium) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getStadiums(): List<Stadium> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getStadium(id: UUID): Flow<Stadium?> {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateStadium(stadium: Stadium) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateStadiumTitle(stadiumId: UUID, title: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteStadium(stadium: Stadium) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLeague(id: UUID): Flow<League?> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLeagues(): List<League> {
+        TODO("Not yet implemented")
+    }
+
+    override fun addLeague(league: League) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateLeague(league: League) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateLeagueTitle(leagueId: UUID, title: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteLeague(league: League) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTeam(id: UUID): Flow<Team?> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTeams(): List<Team> {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteTeam(team: Team) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateTeamName(teamId: UUID, name: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun addTeam(team: Team) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getReferee(id: UUID): Flow<Referee?> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getReferees(): List<Referee> {
+        TODO("Not yet implemented")
+    }
+
+    override fun addReferee(referee: Referee) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteReferee(referee: Referee) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateRefereeFirstName(refereeId: UUID, firstName: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateRefereeMiddleName(refereeId: UUID, middleName: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateRefereeLastName(refereeId: UUID, lastName: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getWeathersList(): Call<WeatherResponse> {
+        TODO("Not yet implemented")
+    }
+}
